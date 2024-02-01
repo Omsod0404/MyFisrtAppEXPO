5c6df5fcb0cfd3424d267e5549f82bb87e7b688e
Omsod0404 <garciaomarucho@gmail.com>
2024-01-31 19:26:37 -0600

    First App

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+const App = () => {
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHelloWorld}>Hola mundo</Text>
+      <Text style={styles.textName}>Me llamo Omar</Text>
+      <Text style={styles.textDescription}>Esta es la materia de Desarrollo de Aplicaciones Móviles</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#102a57',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  textHelloWorld:{
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textName:{
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textDescription:{
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#fff'
+  }
+})
+
+export default App;
